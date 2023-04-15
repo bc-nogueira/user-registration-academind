cb52c273812969ee5f53e39d17eeb81c4fc49784
Breno Nogueira <bcnogueira@id.uff.br>
2023-04-14 21:18:56 -0300

    Commit with the original project.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (username, age) => {
+    setUsersList(prevUsersList => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), name: username, age: age },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+};
+
+export default App;
